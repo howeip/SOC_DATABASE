f09f8a1df79d1cd61cfa2078c283a50bea7b1b1b
howeip <81298271+howeip@users.noreply.github.com>
2025-05-17 11:08:31 +0800

    Create soc_top_upf_cfg.tcl

---
+++ upf/soc_top_upf_cfg.tcl
@@ -0,0 +1,8 @@
+set IS_SOC_FLATTEN    1
+set IS_BBPHY_FLATTEN  1
+set USE_RAM_SIM_MODEL 0
+set IS_CHIPTOP_FLAT   0
+set VCS_SIM           0
+set NETLIST_MEM       0
+set IS_CHIPTOP_FLAT   0
+set IS_HARD_IP        1
