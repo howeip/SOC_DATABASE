26d4bda641e975e5eb4fd34c0e4d3711de663723
howeip <81298271+howeip@users.noreply.github.com>
2025-05-17 11:09:43 +0800

    Update soc_top_upf_cfg.tcl

--- upf/soc_top_upf_cfg.tcl
+++ upf/soc_top_upf_cfg.tcl
@@ -6,3 +6,13 @@ set VCS_SIM           0
 set NETLIST_MEM       0
 set IS_CHIPTOP_FLAT   0
 set IS_HARD_IP        1
+
+#soc_top_upf_nl_cfg.tcl
+set IS_SOC_FLATTEN    1
+set IS_BBPHY_FLATTEN  1
+set USE_RAM_SIM_MODEL 1
+set IS_CHIPTOP_FLAT   0
+set VCS_SIM           0
+set NETLIST_MEM       0
+set IS_CHIPTOP_FLAT   0
+set IS_HARD_IP        1
